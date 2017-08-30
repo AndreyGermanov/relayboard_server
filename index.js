6ed80d310724e8b2ca2fc5f6b6dcdcc1939c41ea
Andrey Germanov <andrey@it-port.ru>
2017-08-30 13:57:34 +0300

    Test serial write

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
 
 // connect to Serial port
-client.connectRTU("/dev/ttyAMA0", { baudrate: 115200 }, write);
-client.setID(1);
+client.connectRTU("/dev/ttyAMA0", { baudrate: 115200 }, {
+    client.setID(1);
+    // Turn ON relay on digital pin 4
+    client.writeRegisters(4,1, function() {
+        // Turn OFF relay on digital pin 4
+        client.writeRegisters(4,0);
+    });
+});
 
-// Turn ON relay on digital pin 4
-client.writeRegisters(4,1);
