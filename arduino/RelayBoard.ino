94c3634f056f0223a3d5bd1145c05c50f2966981
Andrey Germanov <andrey@it-port.ru>
2017-09-28 22:38:04 +0300

    Updated relayboard sketch, added time manipulation library

--- arduino/RelayBoard.ino
+++ arduino/RelayBoard.ino
@@ -1,24 +1,33 @@
+#include <DHT.h>
+
 String incomingLine;
 String request_id,command,argument;
 int stringIndex;
 int statuses[12];
 String pin_types[12] = {"0","0","0","0","0","0","0","0","0","0","0","0"};
 String tmp_array[12] = {"0","0","0","0","0","0","0","0","0","0","0","0"};
+DHT dht;
 
 void printStatuses() {
-  Serial.print(request_id);
-  Serial.print(" ");
+    if (request_id.length()>0) {
+      Serial.print(request_id);
+      Serial.print(" ");
+    }
   Serial.print("STATUS ");
   for (int i=0;i<12;i++) {
-    if (pin_types[i] != "0") {
+    if (pin_types[i] == "relay") {
       Serial.print(statuses[i]);
+    } else if (pin_types[i] == "temperature") {
+      Serial.print(dht.getTemperature());
+      Serial.print("|");
+      Serial.print(dht.getHumidity());
     }
     if (i==11) {
       break;
-    }
-    if (pin_types[i] != "0") {
+    };
+    if (pin_types[i] != "0" && pin_types[i+1] != "0") {
       Serial.print(",");
-    }
+    };
   }
   Serial.println();
 }
@@ -53,6 +62,12 @@ void split(String str,String delimiter) {
     new_pos = str.indexOf(delimiter,pos);
     tmp_array[counter] = str.substring(pos,new_pos);
     pos = new_pos+1;
+    counter++;
+  }
+  String last_str = str.substring(pos);
+  last_str.trim();
+  if (last_str.length()>0) {
+    tmp_array[counter] = str.substring(pos);
   }
 }
 
@@ -60,19 +75,22 @@ void setPinConfig(String config) {
   String current_pin_def[12];
   split(config,",");
   String pin_config[12] = tmp_array;
-  for (int i=0;i<13;i++) {
-    if (pin_config[i] != "0") {
+  for (int i=0;i<12;i++) {
+    pin_config[i].trim();
+    if (pin_config[i] != "0" && pin_config[i] != "") {
       split(pin_config[i],"|");
       pin_types[tmp_array[0].toInt()-1] = tmp_array[1];
-    } else {
-      pin_types[tmp_array[0].toInt()-1] = "0";
+      if (tmp_array[1] == "temperature") {
+        dht.setup(tmp_array[0].toInt());
+      }
     }
   }
 }
 
 void loop()
 {
-    if (Serial.available()>0) {
+  delay(dht.getMinimumSamplingPeriod());
+  if (Serial.available()>0) {
       incomingLine = Serial.readString();
       incomingLine.trim();
       stringIndex = incomingLine.indexOf(" ");
@@ -91,8 +109,10 @@ void loop()
       } else if (command == "OFF") {
         switchRelay(argument.toInt(),HIGH);
       } else if (command == "CONFIG") {
+        Serial.print("SET PIN CONFIG");
         setPinConfig(argument);
       }
     }
     printStatuses();
+    request_id="";
 }
